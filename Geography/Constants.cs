6cd9dc1a0e174cddbaf0c81afd8a96ea5ca48a17
Ainur Hamitov <hamitovainur325@gmail.com>
2025-10-31 16:06:13 +0500

    рефакторинг: вынес магические числа в класс Constants

---
+++ Geography/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geography
+{
+    public static class Constants
+    {
+        public const double DegreesToRadians = Math.PI / 180;
+        public const double RadiansToDegrees = 180 / Math.PI;
+        public const double EarthRadiusKm = 6371;
+
+        public const double KilometersToMiles = 0.621371;
+        public const double MilesToKilometers = 1.60934;
+        public const double MetersToFeet = 3.28084;
+        public const double FeetToMeters = 0.3048;
+        public const double HectaresToAcres = 2.47105;
+        public const double AcresToHectares = 0.404686;
+    }
+}
